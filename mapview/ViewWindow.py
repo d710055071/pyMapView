d064daa7d1999894b310e16741f9063e461d93bb
dongzf <dongzf@meixing.com>
2020-04-06 20:41:38 +0800

    [变更内容]:添加描画类和描画线程类 [ID]:无 [Author]:dongzf [备注]:无

--- mapview/ViewWindow.py
+++ mapview/ViewWindow.py
@@ -2,27 +2,33 @@
 
 from PyQt5.QtWidgets import QWidget
 
+
+from PyQt5.QtGui import QPainter
+
 class ViewWindow(QWidget):
     def __init__(self):
         super(ViewWindow,self).__init__()
-        # test code
-        self.paint_count = 0
-        self.mouse_cont  = 0
 
+        # 设置鼠标跟踪事件 
+        # 只有该函数设置为True鼠标才会实时接收到消息 
+        # 否则只有鼠标按下移动才会收到消息
+        self.setMouseTracking(True)
+
+    def test_paint(self,event):
+        #QPainter
+        pass
     def paintEvent(self,event):
         """
             描画事件
         """
-        # test code
-        self.paint_count += 1
-        print("paintEvent:%d" % self.paint_count)
-        pass
-    def mouseMoveEvent(self,mouseMoveEvent):
+        # 调试代码
+        self.test_paint(event)
+        
+        # 继续向上传递消息
+        event.accept()
+    def mouseMoveEvent(self,event):
         """
-            鼠标移动事件
+            鼠标事件
         """
-        # test code
-        self.mouse_cont += 1
-        print("mouseMoveEvent:%d" % self.mouse_cont)
-
-        pass
+        # 继续向上传递消息
+        event.accept()
