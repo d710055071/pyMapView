89afb2a868ebe828ed871fffbe96eef6b984fe18
dongzf <dongzf@meixing.com>
2020-04-09 22:54:07 +0800

    [变更内容]:添加 信号 槽函数 例子 [ID]:无 [Author]:dongzf [备注]:无

--- core/Painter/PainterThread.py
+++ core/Painter/PainterThread.py
@@ -1,15 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import time
+from queue import Queue
 
 from PyQt5.QtCore import QThread
-
-
+from PyQt5.QtCore import QMutex
 class PainterThread(QThread):
     """
         描画线程类
     """
     def __init__(self):
+        """
+            构造函数
+        """
+        # 调用父类构造函数
         super(PainterThread,self).__init__()
-
+        # 控制线程状态
+        self._run_state = False
+        # 线程内部运行事件队列
+        self._event_list = Queue()
+        # 定义线程锁
+        self._mutex = QMutex()
+    def stop(self):
+        """
+            停止线程函数
+        """
+        self._run_state = True
     def run(self):
-        pass
+        """
+            重载 线程运行函数
+        """
+        while True:
+            # 判断是否退出线程
+            if self._run_state :
+                break
+            # 判断是否有需要处理的事情
+            if self._event_list.empty():
+                time.sleep(1)
+                continue
+            # 弹出队列中最前面的事件进行处理
+            current_event = self._event_list.get()
+
+            # TODO 处理事情
+
--- mapview/ViewWindow.py
+++ mapview/ViewWindow.py
@@ -4,9 +4,17 @@
 from PyQt5.QtWidgets import QWidget
 
 # 
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui  import QPainter
+from PyQt5.QtGui  import QPen
+from PyQt5.QtGui  import QBrush
+from PyQt5.QtCore import QRect
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QPoint
+
 # 导入信号 定义信号使用
 from PyQt5.QtCore import pyqtSignal
+
+#from core.Painter.PainterThread import PainterThread
 
 class ViewWindow(QWidget):
     # 定义信号
@@ -20,22 +28,20 @@ class ViewWindow(QWidget):
         # 只有该函数设置为True鼠标才会实时接收到消息 
         # 否则只有鼠标按下移动才会收到消息
         self.setMouseTracking(True)
-
+        #a = PainterThread()
         # 连接信号与槽函数
-        self.all_connect()
+        #self.all_connect()
 
         # 发送信号
-        self.test_signal.emit()
-    # 定义槽函数
-    def test_func(self):
-        print("test_func")
+        #self.test_signal.emit()
 
     def all_connect(self):
+        """
+            连接该类所以的信号与槽函数
+        """
+        
         self.test_signal.connect(self.test_func)
 
-    def test_paint(self,event):
-        #QPainter
-        pass
     def paintEvent(self,event):
         """
             描画事件
@@ -51,3 +57,41 @@ class ViewWindow(QWidget):
         """
         # 继续向上传递消息
         event.accept()
+
+    def test_paint(self,event):
+        #QPainter
+        # 练习画图的例子
+        p1 = QPoint()
+        point_list = []
+
+
+
+        w = self.width()                    # 绘图区的宽度
+        h = self.height()                   # 绘图区的高度
+
+        rect = QRect(w/4,h/4,w/2,h/2)       # 中间区域矩形框
+
+        # 设置画笔
+        pen = QPen()
+        pen.setWidth(3)                     # 线宽
+        pen.setColor(Qt.red)                # 划线颜色
+        pen.setStyle(Qt.SolidLine)          # 线的样式
+        pen.setCapStyle(Qt.FlatCap)         # 线端点式样
+        pen.setJoinStyle(Qt.BevelJoin)      # 线的连接点样式
+        # 设置画刷
+        brush = QBrush()
+        brush.setColor(Qt.yellow)
+        brush.setStyle(Qt.SolidPattern)
+        painter = QPainter()
+
+        painter.begin(self)
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        painter.drawRect(rect)
+
+        painter.end()
+
+        pass
+    # 定义槽函数
+    #def test_func(self):
+        #print("test_func")
